c990ad02f729f61094a8b2bfa8e238645b942dab
macbookpro <macbookpro@192.168.1.35>
2015-11-20 08:42:28 +0100

    Creando_conexion

---
+++ ScheduleManager/src/model/ConnectionDB.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.*;
+
+/**
+ *
+ * @author macbookpro
+ */
+public class ConnectionDB {
+    public ConnectionDB(){
+    }
+    
+    public Connection getConnection(){
+       Connection con = null;
+       try{
+           Class.forName("oracle.jdbc.driver.OracleDriver").newInstance();
+           con = DriverManager.getConnection("jdbc:oracle:thin:scott/tiger@10.137.2.21:1521:HPUXMA");
+       }catch(SQLException ex){
+       }catch(Exception e){
+       }
+       return con;
+    }
+   
+}
