ec92175db051624804835bae9cb14a4c1efd7fde
aitorpagan <aitorpagan@mbp-de-aitor>
2015-11-26 16:04:14 +0100

    Añadido finalize a clases DAO para cerrar conexion

--- ScheduleManager/src/model/TitulationDAO.java
+++ ScheduleManager/src/model/TitulationDAO.java
@@ -64,4 +64,10 @@ public class TitulationDAO {
         
         return titulations;
     }
+    
+    protected void finalize() throws Throwable{
+            super.finalize();
+            ConnectionDB.closeConnection();
+            
+        }
 }
